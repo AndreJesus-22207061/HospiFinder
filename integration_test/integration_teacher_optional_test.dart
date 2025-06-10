72e0127b4a64d1144fa0c55b9a8f9df793b65f2b
Tomás Nave <127102331+TomasNave-a22208623@users.noreply.github.com>
2025-06-10 23:24:03 +0100

    Delete integration_test directory

--- integration_test/integration_teacher_optional_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:integration_test/integration_test.dart';
-
-import '../test/widget_teacher_optional_test.dart';
-
-void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-  runWidgetTests();
-}
